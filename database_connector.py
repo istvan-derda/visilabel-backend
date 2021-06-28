72765e868f7ace37f8fd859285ec0fd665ac3cfa
István Derda <isde@spreadshirt.net>
2021-06-28 14:51:55 +0200

    bump impl_id

--- database_connector.py
+++ database_connector.py
@@ -32,7 +32,7 @@ class DAO:
                 INSERT INTO rated_configurations (user_id, design_id, background_color, rating, impl_id)
                 VALUES (%s, %s, %s, %s, %s)
                 """
-            data = (user_id, design_id, background_color, rating, 3)
+            data = (user_id, design_id, background_color, rating, 4)
             result = cursor.execute(SQL, data)
             print(result)
             self.conn.commit()
